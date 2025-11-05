093de33b7b96f3e447f526c664a5788e901e5fd2
Aleksandr Shchilkin <shchilkin@gmail.com>
2025-11-05 10:54:46 +0000

    remove Progress Bar

--- src/components/EventCard/index.ts
+++ src/components/EventCard/index.ts
@@ -1,5 +1,4 @@
 export { EventCard } from './EventCard';
 export { AvailabilityBadge } from './AvailabilityBadge';
-export { ProgressBar } from './ProgressBar';
 export { DateTime } from './DateTime';
 export { Location } from './Location';
